67b54b229c6c86084d12e9f8203a7cc98d6aa8fd
Donald Lee <donaldturinglee@gmail.com>
2024-02-04 08:17:29 -0800

    feat: Add exercise01_04.cc

---
+++ chapter_01/exercise01_04.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "a      a^2    a^3 \n";
+    std::cout << 1 << "      " << 1 * 1 << "      " << 1 * 1 * 1 << '\n';
+    std::cout << 2 << "      " << 2 * 2 << "      " << 2 * 2 * 2 << '\n';
+    std::cout << 3 << "      " << 3 * 3 << "      " << 3 * 3 * 3 << '\n';
+    std::cout << 4 << "      " << 4 * 4 << "     " << 4 * 4 * 4 << '\n';
+
+    return 0;
+}
