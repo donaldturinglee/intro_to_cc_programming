fa612f505e43d2291dcf4f18ca34a486d57f36c2
Donald Lee <donaldturinglee@gmail.com>
2024-02-04 07:36:05 -0800

    feat: Add exercise01_03.cc

---
+++ chapter_01/exercise01_03.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "  CCCC    +        +     \n";
+    std::cout << " C        +        +     \n";
+    std::cout << "C      +++++++  +++++++  \n";
+    std::cout << " C        +        +     \n";
+    std::cout << "  CCCC    +        +     \n";
+
+    return 0;
+}
