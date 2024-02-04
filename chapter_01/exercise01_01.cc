9b99a5c40a123fac5e606ee319e1989a4739d9d6
Donald Lee <donaldturinglee@gmail.com>
2024-02-04 07:26:34 -0800

    feat: Add exercise01_01.cc

---
+++ chapter_01/exercise01_01.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "Welcome to C++\n";
+    std::cout << "Welcome to Computer Science\n";
+    std::cout << "Programming is fun\n";
+
+    return 0;
+}
