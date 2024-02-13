29995953bbc69b51a7fc763202ea538dc396161b
Donald Lee <donaldturinglee@gmail.com>
2024-02-12 23:44:23 -0800

    feat: Add exercise01_10.cc

---
+++ chapter_01/exercise01_10.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "The average speed is: " << (14.0 / 1.6) / (45.5 / 60) << " miles per hour" << '\n';
+
+    return 0;
+}
