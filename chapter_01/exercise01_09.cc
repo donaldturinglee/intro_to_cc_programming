5aaa050e504a5f77764f54427d11f240618e122b
Donald Lee <donaldturinglee@gmail.com>
2024-02-12 23:38:19 -0800

    feat: Add exercise01_09.cc

---
+++ chapter_01/exercise01_09.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    std::cout << "The rectangle perimeter is: " << 2 * (4.5 + 7.9) << '\n';
+    std::cout << "The rectangle area is: " << 4.5 * 7.9 << '\n';
+
+    return 0;
+}
