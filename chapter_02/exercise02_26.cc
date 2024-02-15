dca3b77433a2c9f0139d954c1280949978608586
Donald Lee <donaldturinglee@gmail.com>
2024-02-15 01:00:35 -0800

    feat: Add exercise02_26.cc

---
+++ chapter_02/exercise02_26.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+    const double g { 9.8 };
+
+    std::cout << "Enter the friction force in Newtons: ";
+    double F;
+    std::cin >> F;
+
+    std::cout << "Enter the object's mass in kg: ";
+    double m;
+    std::cin >> m;
+
+    std::cout << "Enter the object's acceleration in m/s^2: ";
+    double a;
+    std::cin >> a;
+
+    double coefficient_of_friction { F / (m * g) - a / g };
+    std::cout << "The coefficient for friction is " << coefficient_of_friction << '\n';
+
+    return 0;
+}
